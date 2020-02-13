4f894071f462c357db10cb65a1c123ab601d1c78
stefanG41 <47351072+stefanG41@users.noreply.github.com>
2020-02-13 20:53:29 +0100

    Update setup_pi_docker.sh

--- setup_pi_docker.sh
+++ setup_pi_docker.sh
@@ -1,10 +1,22 @@
+1_update_and_upgrade_pi ()
+
+{
 sudo apt-get update && sudo apt-get upgrade -y
-sudo remove libvlc-bin -y
-sudo apt remove libvlc-bin -y
-sudo apt remove vlc-plugin-qt -y
-sudo apt-get update && sudo apt-get upgrade -y
+}
+
+2_change_hostname ()
+
+{
+sudo hostname docker
+}
+
+
+3_
 sudo nano /etc/hostname
 sudo nano /etc/hosts
-sudo reboot
+
 sudo curl -fsSL get.docker.com -o get-docker.sh && sh get-docker.sh
 sudo apt-get install docker-compose -y
+sudo usermod -aG docker pi
+
+sudo reboot
