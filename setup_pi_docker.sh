71a4593b7b7b91ab8c0a6a19fd47377150fdcf41
stefanG41 <47351072+stefanG41@users.noreply.github.com>
2020-02-13 20:33:25 +0100

    Create setup_pi_docker.sh

---
+++ setup_pi_docker.sh
@@ -0,0 +1,10 @@
+sudo apt-get update && sudo apt-get upgrade -y
+sudo remove libvlc-bin -y
+sudo apt remove libvlc-bin -y
+sudo apt remove vlc-plugin-qt -y
+sudo apt-get update && sudo apt-get upgrade -y
+sudo nano /etc/hostname
+sudo nano /etc/hosts
+sudo reboot
+
+sudo apt-get install docker docker-compose -y
