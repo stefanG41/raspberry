5bee4e45d40039cf3ec1a7d6f18e803e9e878041
stefanG41 <47351072+stefanG41@users.noreply.github.com>
2020-02-20 23:14:20 +0100

    Create iobroker_adapter.sh

---
+++ iobroker_adapter.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+MY_HOSTNAME=`uname -n`
+
+cd /opt/iobroker
+./iobroker add zigbee  --host $MY_HOSTNAME
+./iobroker add tr-064  --host $MY_HOSTNAME
+./iobroker add backitup  --host $MY_HOSTNAME
+./iobroker add alexa2  --host $MY_HOSTNAME
+./iobroker add ping  --host $MY_HOSTNAME
+./iobroker add fritzdect  --host $MY_HOSTNAME
+./iobroker add bosesoundtouch  --host $MY_HOSTNAME
+./iobroker add harmony  --host $MY_HOSTNAME
+./iobroker add samsung  --host $MY_HOSTNAME
+./iobroker add shelly  --host $MY_HOSTNAME
+./iobroker add synology  --host $MY_HOSTNAME
+./iobroker add telegram  --host $MY_HOSTNAME
+./iobroker add vis  --host $MY_HOSTNAME
